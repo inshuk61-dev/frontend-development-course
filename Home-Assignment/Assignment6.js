57a76db82254619970be0225db8af3abf70404b5
inshu <inshuk61@gmail.com>
2026-08-05 19:27:24 +0500

    Assignmrnt in IF ELSE condition

---
+++ Home-Assignment/Assignment6.js
@@ -0,0 +1,50 @@
+// Pass or fail
+let marks=50;
+ if (marks>=60){
+    console.log("Pass")
+ }
+ else{
+    console.log("Fail")
+ }
+
+//Positive, Negative or Zero 
+
+let number=-4;
+if(number>0){
+    console.log("Positive")
+}
+ else if (number<0){
+    console.log("Negative")
+}
+else
+{
+    console.log("Zero")
+}
+
+// Eligible for NIC
+let age=18;
+if (age>=18){
+    console.log("You're eligible for CNIC")
+}
+else{
+    console.log("You're not eligible")
+}
+
+// Largest of Two numbers
+let a=10;
+let b=30;
+if (a>b){
+    console.log("Firsr number is larger")
+}
+else{
+    console.log("Second number is larger")
+}
+
+// Even or Odd 
+let c=32;
+if (c%2===0){
+    console.log("Even")
+}
+else{ 
+    console.log("Odd")
+}
